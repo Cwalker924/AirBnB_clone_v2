e6a9c1c0f8e068218b3a321e87e3e27ac057f0bd
Cwalker924 <cwalker924@gmail.com>
2017-03-29 13:39:25 +0000

    adding State.py

--- models/state.py
+++ models/state.py
@@ -11,19 +11,19 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     '''This is the 'State' class'''
-    if Base is not object:
+    if getenv("HBNB_TYPE_STORAGE", "fs") == db:
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="all, delete, delete-orphan")
     else:
         name = ""
-        cities = [""]
+        @property
+        def cities(self):
+            cities = models.storage.all("City").values()
+            result = [city for city in cities if city.state_id == self.id]
+            return (result)
 
     def __init__(self, *args, **kwargs):
         '''This is the initialization method'''
         super(State, self).__init__(*args, **kwargs)
-
-    if Base is object:
-        def cities(self):
-            storage.all('City')
