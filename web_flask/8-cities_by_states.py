b821b709c731de16bccc816d04d78effdf110a97
Cwalker924 <cwalker924@gmail.com>
2017-03-29 22:47:44 +0000

    adding 9.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 
 
 def show_state():
-    my_list = storage.all("State").values()
-    return render_template('7-states_list.html', my_list=my_list)
+    states_l = storage.all("State").values()
+    return render_template('7-states_list.html', states_l=states_l)
 
 
 @app.route('/cities_by_states')
 def cities_by_states_html():
-    my_list = storage.all("State").values()
-    return render_template('8-cities_by_states.html', my_list=my_list)
+    states_l = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states_l=states_l)
 
 
 @app.teardown_appcontext
